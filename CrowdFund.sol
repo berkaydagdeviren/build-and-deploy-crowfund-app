01ae85c0f667e90529e6409994c2e07bbd55574b
berkaydagdeviren <76188403+berkaydagdeviren@users.noreply.github.com>
2022-09-19 22:21:14 +0300

    Update CrowdFund.sol

--- CrowdFund.sol
+++ CrowdFund.sol
@@ -6,6 +6,8 @@ import "./IERC20.sol";
 
 contract CrowdFund {
 
+    //events 
+    
     event Launch(
         uint id, 
         address indexed creator,
@@ -46,16 +48,25 @@ contract CrowdFund {
     mapping(uint => Campaign) public campaigns; // campaigns map
     mapping (uint => mapping(address => uint)) public plegedAmount; // users' pledged coins amount
 
+    // construct with IERC20 token
     constructor(address _token){
         token = IERC20(_token);
     }
+    
+    // launc function
 
     function launch(uint _goal, uint32 _startAt, uint32 _endAt) external {
+    
+        //if function is started and not ended 
+        
         require(_startAt >= block.timestamp, "Event has not started yet.");
         require(_endAt >= _startAt, "Event has ended.");
         require(_endAt >= block.timestamp + 90 days, "Event cannot be longer than 90 days");
 
         count += 1;
+        
+        //launch with Campaign struct
+        
         campaigns[count] = Campaign({
             creator: msg.sender,
             goal: _goal,
@@ -67,26 +78,43 @@ contract CrowdFund {
 
         emit Launch(count, msg.sender, _goal, _startAt, _endAt);
     }
-
+    
+    // cancel function
+    
     function cancel(uint _id) external {
         Campaign memory campaign = campaigns[_id];
+        
+        // make sure sender is the creator and campaign has not started
+        
         require(msg.sender == campaign.creator, "Only creator can cancel the event");
         require(block.timestamp < campaign.startAt, "Campaign already started");
         delete campaigns[_id];
         emit Cancel(_id);
     }
-
+    
+    //pledge function
+    
     function pledge(uint _id, uint _amount) external {
          Campaign storage campaign = campaigns[_id];
+         
+         // if campaign started and not ended
+         
          require(block.timestamp >= campaign.startAt, "NOT started");
          require(block.timestamp <= campaign.endAt, "ended");
+         
+         //increment pledged
+         
          campaign.pledged += _amount;
          plegedAmount[_id][msg.sender] += _amount;
+         
+         //transfer token to the creator of fund
          token.transferFrom(msg.sender, address(this), _amount);
 
          emit Pledge(_id, msg.sender, _amount);
     }
-
+    
+    // unpledge function
+    
     function unpledge(uint _id, uint _amount) external {
         Campaign storage campaign = campaigns[_id];
         require(block.timestamp <= campaign.endAt, "ended");
@@ -95,12 +123,18 @@ contract CrowdFund {
 
         emit Unpledge(_id, msg.sender, _amount);
     }
+    
+    // claim pledge function
 
     function claim(uint _id) external {
         Campaign storage campaign = campaigns[_id];
+        
+        // make sure msg sender is the creator and campaign has ended && goal achieved
         require(msg.sender == campaign.creator, "not creator");
         require(block.timestamp >= campaign.endAt, "not ended");
         require(campaign.pledged >= campaign.goal, "goal not achieved");
+        
+        // if not claimed already make it claimed, transfer pledged amount
         require(!campaign.claimed, "claimed");
         campaign.claimed = true;
         token.transfer(msg.sender, campaign.pledged);
@@ -108,15 +142,21 @@ contract CrowdFund {
         emit Claim(_id);
     }
 
+
+    // refund function
     function refund(uint _id) external {
         Campaign storage campaign = campaigns[_id];
+        
+        // campaign must have ended && goald have not reached 
         require(block.timestamp > campaign.endAt, "not ended");
         require(campaign.pledged < campaign.goal, "goal not achieved");
-  
+        
+        // send balance back to the user
         uint bal = pledgedAmount[_id][msg.sender];
         pledgedAmount[_id][msg.sender] = 0;
         token.transfer(msg.sender, bal);
 
+        // launc refund event
         emit Refund(_id, msg.sender, bal);
     }
 }
